8279ec35557f84b6ea0a59cbe9477872f4a0cf16
skymxc <164443895@qq.com>
2018-07-14 09:53:01 +0800

    线程优先级

---
+++ src/com/skymxc/example/multithreading/priority/PriorityTest.java
@@ -0,0 +1,8 @@
+package com.skymxc.example.multithreading.priority;
+
+public class PriorityTest {
+
+    public static void main(String[]args){
+        System.out.println("currentPriority->"+Thread.currentThread().getPriority());
+    }
+}
